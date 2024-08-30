c5ccb71cd05ed80100f82b13effc307c577ac9d8
Nikita <Remeshevskyi.Nikita03@gmail.com>
2024-08-30 18:14:50 +0100

    web transfer

--- test.py
+++
@@ -1,9 +0,0 @@
-from database.db import get_db
-from database.models import HistoryToAnalyze
-
-
-db = next(get_db())  # Get a database session
-stocks = db.query(HistoryToAnalyze).all()
-
-for i in stocks:
-    print(i.title)
